2ee73cb47451717bf5da61310e823f98886676a0
Abhishek More <abhimore74@gmail.com>
2019-02-18 23:31:06 -0500

    Initial Commit

---
+++ WeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeCycle
+//
+//  Created by Abhishek More on 2/18/19.
+//  Copyright © 2019 Abhishek More. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
